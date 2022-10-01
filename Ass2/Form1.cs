e30f2f77845471cb312aa1d7d8dae5474f3c2b36
fatimaAfzaal <fatimaafzaal2002@gmail.com>
2022-10-01 20:38:03 +0500

    Add project files.

---
+++ Ass2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                label5.Text = (double.Parse(textBox1.Text) + double.Parse(textBox2.Text)).ToString();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Please enter a valid number","Invalid Input", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label5.Text = (double.Parse(textBox1.Text) - double.Parse(textBox2.Text)).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter a valid number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label5.Text = (double.Parse(textBox1.Text) * double.Parse(textBox2.Text)).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter a valid number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label5.Text = (double.Parse(textBox1.Text) / double.Parse(textBox2.Text)).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter a valid number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
